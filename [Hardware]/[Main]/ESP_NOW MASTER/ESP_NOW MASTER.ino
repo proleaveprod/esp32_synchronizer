823dc33e177daf6622e2359fa5ca064bbe29f4fb
proleaveprod <proleaveprod@gmail.com>
2022-11-26 20:17:26 +0300

    nahuy esp-now

--- [Hardware]/[Main]/ESP_NOW MASTER/ESP_NOW MASTER.ino
+++ [Hardware]/[Main]/ESP_NOW MASTER/ESP_NOW MASTER.ino
@@ -3,6 +3,7 @@
 
 #define PIN_TEST  21
 #define WIFI_RATE WIFI_PHY_RATE_48M
+
 uint8_t master1Addr[] = {0xC4,0xDE,0xE2,0x1C,0x40,0xCC};
 uint8_t broadcastAddr[] =  {0xFF,0xFF,0xFF,0xFF,0xFF,0xF1};
 esp_now_peer_info_t peerInfo;
@@ -29,9 +30,27 @@ void setup() {
 
   wifi_init();
   
-  Serial.println("Delay 1sec.");
-  delay(1000);
-  master_sync();
+
+  uint8_t a = '@';
+  uint32_t t1=0;
+  uint32_t t2=0; 
+
+  while(1){
+  t1 = micros();
+  
+  
+  
+  esp_now_send(broadcastAddr, &a, 1); // Отправляем 5 байт Resp-пакета
+
+
+  t2 = micros();
+  Serial.print("dt = ");Serial.println(t2-t1);
+  delay(100);
+  }
+
+  // Serial.println("Delay 1sec.");
+  // delay(1000);
+  // master_sync();
 
 }
  
@@ -75,13 +94,6 @@ void master_sync(){
 
 }
 
-void send_sync(){
-  t1_sync = micros(); // Засекаем время
-  uint8_t dataToSend[5] = {'$',0,0,0,0};       
-  memcpy(&dataToSend[1], &t1_sync, 4);  // Копируем в dataToSend 4 байта с t1
-  esp_now_send(broadcastAddr, dataToSend, 5); // Отправляем 5 байт Sync-пакета 
-}
-
 void OnMasterRecvPTP(const uint8_t * mac, const uint8_t *incomingData, int len){
   uint8_t recData=0;
   memcpy(&recData, incomingData, 1);
@@ -98,6 +110,16 @@ void OnMasterRecvPTP(const uint8_t * mac, const uint8_t *incomingData, int len){
 
 }
 
+
+void send_sync(){
+  t1_sync = micros(); // Засекаем время
+  uint8_t dataToSend[5] = {'$',0,0,0,0};       
+  memcpy(&dataToSend[1], &t1_sync, 4);  // Копируем в dataToSend 4 байта с t1
+  esp_now_send(broadcastAddr, dataToSend, 5); // Отправляем 5 байт Sync-пакета 
+}
+
+
+
 /*void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
     digitalWrite(PIN_TEST,1);
     long time_d = micros();
@@ -107,14 +129,25 @@ void OnMasterRecvPTP(const uint8_t * mac, const uint8_t *incomingData, int len){
 */
 
 void wifi_init(){
+
+
+
+
   Serial.println("- ESP-NOW init -");
+  
+  
+  
   WiFi.mode(WIFI_STA);
+
+
+
+  
   if (esp_now_init() != ESP_OK) {
     Serial.println("Error initializing ESP-NOW");
     return;
   }
 
-  esp_wifi_config_espnow_rate(WIFI_IF_STA,WIFI_RATE);
+  //esp_wifi_config_espnow_rate(WIFI_IF_STA,WIFI_RATE);
   peerInfo.channel = 0;  
   peerInfo.encrypt = false; 
   memcpy(peerInfo.peer_addr, broadcastAddr, 6);       
--- [Hardware]/[Main]/ESP_NOW SLAVE/ESP_NOW SLAVE.ino
+++ [Hardware]/[Main]/ESP_NOW SLAVE/ESP_NOW SLAVE.ino
@@ -15,33 +15,37 @@ uint8_t pin_state=0;
 uint8_t tim_flag=0;
 
 // callback function that will be executed when data is received
-void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
-  char recData;
-  memcpy(&recData, incomingData, sizeof(recData));
 
 
-  if(recData=='@'){
-    
-      digitalWrite(PIN_TEST,1);
-      long time_d = micros();
-      while(micros()-time_d<3000);
-      digitalWrite(PIN_TEST,0);
+TaskHandle_t Task1;
 
 
-  }
-}
+
+
  
 void setup() {
   // Initialize Serial Monitor
   Serial.begin(115200);
   pinMode(PIN_TEST,OUTPUT);
-
-
+  
+  // xTaskCreatePinnedToCore(
+  // Task1code, /* Функция, содержащая код задачи */
+  // "Task1", /* Название задачи */
+  // 10000, /* Размер стека в словах */
+  // NULL, /* Параметр создаваемой задачи */
+  // 3, /* Приоритет задачи */
+  // &Task1, /* Идентификатор задачи */
+  // 0); /* Ядро, на котором будет выполняться задача */
+  
+  
   //timer_init();
-  Serial.println("Delay 1sec.");
-  delay(1000);
+  // Serial.println("Delay 1sec.");
+  // delay(10000);
   slave_wifi_init();  
-  slave_sync();
+
+  esp_now_register_recv_cb(OnSlaveRecv1byte);
+
+  //slave_sync();
 
   // esp_now_register_recv_cb(OnDataRecv);
 
@@ -51,21 +55,66 @@ void loop() {
   delay(1000);  // Нужно, чтобы не ебашило таймер.
 }
 
-void IRAM_ATTR onTimer(){
 
-  pin_state = !pin_state;
 
-  digitalWrite(PIN_TEST, pin_state);   // Блинкуем пин
+
+void Task1code( void * pvParameters ){
+  Serial.print("Task1 running on core");
+  Serial.println(xPortGetCoreID());
+           //  "Задача Task1 выполняется на ядре "
+  
+
+  for(;;){
+    digitalWrite(PIN_TEST, HIGH);
+    delay(1000);
+    digitalWrite(PIN_TEST, LOW);
+    delay(1000);
+  } 
 }
 
-void timer_init(){
-  Serial.println("- TIMER init -");
-  My_timer = timerBegin(3, 8, true);
-  timerAttachInterrupt(My_timer, &onTimer, true);
-  timerAlarmWrite(My_timer, 100, true);          //Раз в миллисек
-  timerAlarmEnable(My_timer);
 
+
+
+
+
+
+
+
+
+
+
+uint32_t t1=0;
+uint32_t t2=0;
+void OnSlaveRecv1byte(const uint8_t * mac, const uint8_t *incomingData, int len){
+  uint8_t recData=0;
+  memcpy(&recData, incomingData, 1);  // Принимаем 5 байт с таймингом
+  t1 = micros();
+
+  //Serial.println(xPortGetCoreID());
+  Serial.println(t1-t2);
+
+  t2 = micros();
+
+  // digitalWrite(PIN_TEST,1);
+  //     long time_d = micros();
+  //     while(micros()-time_d<3000);
+  //     digitalWrite(PIN_TEST,0);
 }
+// void IRAM_ATTR onTimer(){
+
+//   pin_state = !pin_state;
+
+//   digitalWrite(PIN_TEST, pin_state);   // Блинкуем пин
+// }
+
+// void timer_init(){
+//   Serial.println("- TIMER init -");
+//   My_timer = timerBegin(3, 8, true);
+//   timerAttachInterrupt(My_timer, &onTimer, true);
+//   timerAlarmWrite(My_timer, 100, true);          //Раз в миллисек
+//   timerAlarmEnable(My_timer);
+
+// }
 
 void slave_sync(){
   Serial.println("Slave sync start");
@@ -101,9 +150,9 @@ void OnSlaveRecvPTP(const uint8_t * mac, const uint8_t *incomingData, int len){
     deltaSync_counter++;
     Serial.print(deltaSync_counter);Serial.print(". ");
 
-    // Serial.print("deltaSync = ");
+     Serial.print("deltaSync = ");
     Serial.println(deltaSync%100000);
-    //Serial.print("time for sync = ");Serial.println(t4_resp-t1_sync);
+    Serial.print("time for sync = ");Serial.println(t4_resp-t1_sync);
     // Serial.print("    t1=");Serial.println(t1_sync);
     // Serial.print("    t4=");Serial.println(t4_resp);
     // Serial.print("    t2=");Serial.println(t2_getSync);
@@ -131,7 +180,7 @@ void slave_wifi_init(){
     Serial.println("Error initializing ESP-NOW");
     return;
   }
-  esp_wifi_config_espnow_rate(WIFI_IF_STA,WIFI_RATE);
+  //esp_wifi_config_espnow_rate(WIFI_IF_STA,WIFI_RATE);
   peerInfo.channel = 0;  
   peerInfo.encrypt = false; 
   memcpy(peerInfo.peer_addr, master1Addr, 6); 
